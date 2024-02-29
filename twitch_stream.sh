cdf8e86dd5d34946a61878617852f3fdb0b2ac9d
taxorbot <taxorbot@gmail.com>
2024-02-29 06:59:42 +0100

    Small fix

--- twitch_stream.sh
+++ twitch_stream.sh
@@ -3,5 +3,7 @@
 KEY="live_487176421_EcsFu6ZRH7WfHD5L72cobItWDTQjcQ"
 VIDEO_PATH="videolist.txt"
 AUDIO_PATH="audiolist.txt"
+WORK_DIR="/root/config/streamer/"
 
+cd $WORK_DIR
 ffmpeg -re -f concat -safe 0 -i $VIDEO_PATH -f concat -i $AUDIO_PATH -c:v copy -c:a copy -f flv -g 60 -flvflags no_duration_filesize $URL/$KEY
