f7e5c1d15032e2e1cbcbca91645fa0d553800dc4
taxorbot <taxorbot@gmail.com>
2024-02-29 10:08:14 +0100

    Small fix

--- youtube_stream.sh
+++ youtube_stream.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
-KEY="jww0-gred-8vfj-tduk-fa13"
 VIDEO_PATH="videolist.txt"
 AUDIO_PATH="audiolist.txt"
-#AUDIO_PATH="video/test.mp3"
-#VIDEO_PATH="video/stream_base.mp4"
+WORK_DIR="/root/config/streamer/"
+
+KEY="jww0-gred-8vfj-tduk-fa13"
 URL="rtmp://a.rtmp.youtube.com/live2"
 
-ffmpeg -re -stream_loop -1 -f concat -i $VIDEO_PATH -f concat -i $AUDIO_PATH -c:v copy -c:a copy -f flv -g 60 -flvflags no_duration_filesize $URL/$KEY
+cd $WORK_DIR
+ffmpeg -re -f concat -safe 0 -i $VIDEO_PATH -f concat -i $AUDIO_PATH -c:v copy -c:a copy -f flv -g 60 -flvflags no_duration_filesize $URL/$KEY
