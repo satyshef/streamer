ea0419d18f7696ba7fcf769705fd6593af026e1c
taxorbot <taxorbot@gmail.com>
2024-03-09 06:56:09 +0100

    first test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-import libs.youtube.stream as youtube
+import lib.youtube.stream as youtube
 
 TITLE = "СВОДКА НОВОСТЕЙ"
 DESCRIPTION = "Самые актуальные новости на данный момент"
-THUMBNAIL_FILE = "data/masa_stream.png"
+THUMBNAIL_FILE = "youtube_streamer/masa_stream.png"
 
 if __name__ == '__main__':
-    ingestion = youtube.create_stream(TITLE, DESCRIPTION, THUMBNAIL_FILE)
+    ingestion = youtube.create_stream(TITLE, DESCRIPTION, THUMBNAIL_FILE, "public")
     print(ingestion)
     
    
